08b3b6ccb56c5e33f87e4bde94aa979931eb60b2
Gokul K <kcgokul1995@gmail.com>
2020-08-23 00:03:40 +0530

    checking with https

--- docker-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker ps -a awk '{ print \$1,\$2 }' grep gokuldevops/hello-world awk '{print \$1 }' xargs -I {} docker rm -f {}
-docker run -d -p 8080:8080 gokuldevops/hello-world:latest
