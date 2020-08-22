793e08450f3d81f9a6f8b373404700afdcd4b9ba
Gokul K <kcgokul1995@gmail.com>
2020-08-22 22:29:19 +0530

    correcting the mistake of docker repo url

--- docker-run.sh
+++ docker-run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker ps -a awk '{ print \$1,\$2 }' grep gokul0815/hello-world awk '{print \$1 }' xargs -I {} docker rm -f {}
-docker run -d -p 8080:8080 gokul0815/hello-world:latest
+docker ps -a awk '{ print \$1,\$2 }' grep gokuldevops/hello-world awk '{print \$1 }' xargs -I {} docker rm -f {}
+docker run -d -p 8080:8080 gokuldevops/hello-world:latest
